8bda50ad7a4296f7c0de7927f6499631ff77914f
Lahiru De Silva <lahirude@wso2.com>
2026-02-17 18:41:43 +0530

    chore: enable modules only for non-released docs
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

--- src/theme/NavbarItem/ComponentTypes.tsx
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -1,9 +1,11 @@
 import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
 import GitHubStars from '@site/src/components/common/GitHubStars';
+import VersionAwareModulesLink from '@site/src/components/common/VersionAwareModulesLink';
 
 // Refer: https://github.com/facebook/docusaurus/issues/7227
 
 export default {
   ...ComponentTypes,
   'custom-gitHubStars': GitHubStars,
+  'custom-versionAwareModulesLink': VersionAwareModulesLink,
 };
