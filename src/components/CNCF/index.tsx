bb403fbd2bc69b104d638313532cdb197fa88361
Sameera Jayasoma <sameera.madushan@gmail.com>
2026-03-17 12:06:01 -0700

    Update to switch CNCF log based on the theme
    Signed-off-by: Sameera Jayasoma <sameera.madushan@gmail.com>

--- src/components/CNCF/index.tsx
+++ src/components/CNCF/index.tsx
@@ -1,6 +1,7 @@
 import type {ReactNode} from 'react';
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import ThemedImage from '@theme/ThemedImage';
 import styles from './styles.module.css';
 
 /**
@@ -11,17 +12,20 @@ export default function CNCF(): ReactNode {
   return (
     <section className={styles.section}>
       <div className="container">
-        <div className={styles.content}>
-          <p className={styles.text}>
+        <h3 className=""><p className={styles.text}>
             OpenChoreo is a{' '}
             <a href="https://www.cncf.io/" target="_blank" rel="noopener noreferrer">
               CNCF (Cloud Native Computing Foundation)
             </a>{' '} sandbox project.
-          </p>
-          <img
-            src={useBaseUrl('/img/logos/cncf-color.svg')}
+          </p></h3>
+        <div className={styles.content}>
+          <ThemedImage
             alt="CNCF Logo"
             className={styles.logo}
+            sources={{
+              light: useBaseUrl('/img/logos/cncf-color.svg'),
+              dark: useBaseUrl('/img/logos/cncf-white.svg'),
+            }}
           />
           <p className={styles.madeWith}>Made with ❤️ at <a href="https://wso2.com/open-source/" target="_blank" rel="noopener noreferrer">WSO2</a></p>
         </div>
