c1d8b7616bae504e18d5b2c27325fbee9dbf6ff6
yashodgayashan <yashodgayashan@gmail.com>
2026-05-18 20:04:26 +0530

    docs: add v1.1.0 release docs
    Signed-off-by: yashodgayashan <yashodgayashan@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,8 +85,11 @@ const config: Config = {
       'classic',
       {
         docs: {
-          lastVersion: 'v1.0.x',
+          lastVersion: 'v1.1.x',
           versions: {
+            'v1.1.x': {
+              label: 'v1.1.x',
+            },
             'v1.1.0-alpha-1': {
               label: 'v1.1.0-alpha-1',
             },
@@ -141,9 +144,9 @@ const config: Config = {
 
   themeConfig: {
     announcementBar: {
-      id: 'release_v1_0_0',
+      id: 'release_v1_1_0',
       content:
-        '🎉️ OpenChoreo <a target="_blank" rel="noopener noreferrer" href="https://github.com/openchoreo/openchoreo/releases/tag/v1.0.0">v1.0.0</a> has been released! 🎉',
+        '🎉️ OpenChoreo <a target="_blank" rel="noopener noreferrer" href="https://github.com/openchoreo/openchoreo/releases/tag/v1.1.0">v1.1.0</a> has been released! 🎉',
       isCloseable: true,
     },
     docsearch: {
