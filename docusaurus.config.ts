6ea113b8d21c8295f2176fa3ed7858bce7c91810
Kavishka Fernando <kavi@kavishka.local>
2026-07-02 13:33:15 +0530

    add all posts to the sidebar
    Signed-off-by: Kavishka Fernando <kavi@kavishka.local>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -161,6 +161,8 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
